bc180c0a143b0aa0341584f05fdc94630f1a3f49
Shubham Patgavkar <155343288+ShubhamPatgavkar@users.noreply.github.com>
2025-03-29 20:36:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.java
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.java
@@ -1,97 +1,86 @@
 //{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
+// Initial Template for Java
 import java.util.*;
 
-class Job {
-    int id, profit, deadline;
 
-    Job(int x, int y, int z) {
-        this.id = x;
-        this.deadline = y;
-        this.profit = z;
-    }
-}
-
-class GfG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] jobIDStrings = sc.nextLine().split(" ");
-            String[] deadlineStrings = sc.nextLine().split(" ");
-            String[] profitStrings = sc.nextLine().split(" ");
-
-            // Convert the input strings to integer arrays
-            int[] jobIDs = new int[jobIDStrings.length];
-            int[] deadlines = new int[deadlineStrings.length];
-            int[] profits = new int[profitStrings.length];
-
-            for (int i = 0; i < jobIDStrings.length; i++) {
-                jobIDs[i] = Integer.parseInt(jobIDStrings[i]);
-                deadlines[i] = Integer.parseInt(deadlineStrings[i]);
-                profits[i] = Integer.parseInt(profitStrings[i]);
-            }
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.JobSequencing(jobIDs, deadlines, profits);
-            System.out.println(ans.get(0) + " " + ans.get(1));
-            System.out.println("~");
-        }
-    }
-}
 // } Driver Code Ends
 
 
 
+ class Pair implements Comparable<Pair> {
+    int d;
+    int p;
 
-class Solution {
-    static class Job {
-        int id, deadline, profit;
-        Job(int id, int deadline, int profit) {
-            this.id = id;
-            this.deadline = deadline;
-            this.profit = profit;
-        }
+    public Pair(int pro, int d) {
+        this.d = d;
+        this.p = pro;
     }
 
-    public ArrayList<Integer> JobSequencing(int[] id, int[] deadline, int[] profit) {
-        int n = profit.length;
-
-        // Creating a list of jobs
-        Job[] jobs = new Job[n];
-        for (int i = 0; i < n; i++) {
-            jobs[i] = new Job(id[i], deadline[i], profit[i]);
-        }
-
-        // Sort jobs in descending order of profit
-        Arrays.sort(jobs, (a, b) -> b.profit - a.profit);
-
-        int maxDeadline = 0;
-        for (int i = 0; i < n; i++) {
-            maxDeadline = Math.max(maxDeadline, jobs[i].deadline);
-        }
-
-        // Slots for scheduling
-        int[] slot = new int[maxDeadline + 1];
-        Arrays.fill(slot, -1);
-
-        int maxProfit = 0, countJobs = 0;
-        for (Job job : jobs) {
-            for (int j = job.deadline; j > 0; j--) {
-                if (slot[j] == -1) { // Check if slot is available
-                    slot[j] = job.id;
-                    maxProfit += job.profit;
-                    countJobs++;
+    @Override
+    public int compareTo(Pair other) {
+        return Integer.compare(other.p, this.p); // Descending order
+    }
+}
+    
+class Solution {
+   
+    public ArrayList<Integer> jobSequencing(int[] deadline, int[] profit) {
+        
+        int sc[] = new int[profit.length+1];
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        
+        for(int i=0; i<deadline.length; i++){
+            pq.add(new Pair(profit[i], deadline[i]));
+        } 
+        int c = 0;
+        int totalp =0;
+        
+        while(!pq.isEmpty()){
+            Pair p1 = pq.poll();
+            for(int i = p1.d; i>  0; i--){
+                if(sc[i] == 0){
+                    sc[i] = p1.p;
+                    totalp += p1.p;
+                    c++;
                     break;
                 }
             }
         }
-
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(countJobs);
-        result.add(maxProfit);
-        return result;
+        
+        ArrayList<Integer>list = new ArrayList<>();
+        list.add(c);
+        list.add(totalp);
+        return list;
+        
     }
 }
+    
+
+
+
+//{ Driver Code Starts.
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine().trim());
+
+        while (t-- > 0) {
+            String[] deadlineInput = sc.nextLine().trim().split("\\s+");
+            int[] deadline =
+                Arrays.stream(deadlineInput).mapToInt(Integer::parseInt).toArray();
+
+            String[] profitInput = sc.nextLine().trim().split("\\s+");
+            int[] profit =
+                Arrays.stream(profitInput).mapToInt(Integer::parseInt).toArray();
+            Solution obj = new Solution();
+            ArrayList<Integer> result = obj.jobSequencing(deadline, profit);
+            System.out.println(result.get(0) + " " + result.get(1));
+            System.out.println("~");
+        }
+
+        sc.close();
+    }
+}
+// } Driver Code Ends
